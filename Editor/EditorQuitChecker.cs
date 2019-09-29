7d423a90870317a9846cc48b0927b9bc062236e6
baba_s <shotababa1206@gmail.com>
2019-09-29 16:35:46 +0900

    first commit

---
+++ Editor/EditorQuitChecker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace KoganeUnityLib
+{
+	[InitializeOnLoad]
+	internal static class EditorQuitChecker
+	{
+		static EditorQuitChecker()
+		{
+			EditorApplication.wantsToQuit += () =>
+			{
+				var isJapanese = Application.systemLanguage == SystemLanguage.Japanese;
+				var message    = isJapanese ? "Unity を終了しますか？" : "Exit the Unity Editor?";
+				var ok         = isJapanese ? "はい" : "Yes";
+				var cancel     = isJapanese ? "いいえ" : "No";
+
+				return EditorUtility.DisplayDialog( "Unity", message, ok, cancel );
+			};
+		}
+	}
+}
